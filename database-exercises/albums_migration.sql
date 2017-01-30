4aa2214210b476582322fa138298634ffd3a439f
nedwaldie <nw_waldie3@yahoo.com>
2017-01-30 08:46:19 -0600

    Committing MySQL Codeup exercises

--- database-exercises/albums_migration.sql
+++ database-exercises/albums_migration.sql
@@ -4,9 +4,20 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INTEGER ,
-    artist TEXT ,
-    name TEXT ,
+    artist LINESTRING ,
+    name LINESTRING ,
     release_date INTEGER ,
     sales FLOAT ,
     genre TEXT
 );
+
+ALTER TABLE albums ADD UNIQUE (`artist`, `name`);
+
+ALTER TABLE albums
+    MODIFY id INT NOT NULL AUTO_INCREMENT;
+
+DESCRIBE albums;
+
+INSERT INTO albums VALUES(id, 'Michael Jackson', '', 1987, 203000000, 'Pop, funk, rock');
+
+DELETE FROM albums WHERE id >= 34;
--- database-exercises/group_by_exercise.sql
+++ database-exercises/group_by_exercise.sql
@@ -8,3 +8,52 @@ SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY 
 
 # Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY. Your results should be:
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+SELECT concat(e.first_name, ' ', e.last_name) as full_name,
+  d.dept_name,
+  s.salary
+FROM employees as e
+  JOIN dept_emp ON e.emp_no = dept_emp.emp_no
+  JOIN departments as d ON dept_emp.dept_no = d.dept_no
+  JOIN salaries as s ON e.emp_no = s.emp_no
+WHERE d.dept_name = 'Development' AND s.to_date = '9999-01-01';
+
+# shows each department along with the name of the current manager for that department.
+SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) as full_name
+FROM employees AS e
+  JOIN dept_manager as dm ON e.emp_no = dm.emp_no
+  JOIN departments as d ON dm.dept_no = d.dept_no
+WHERE dm.to_date = '9999-01-01';
+
+# Find the name of all departments currently managed by women.
+SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) as full_name
+FROM employees AS e
+  JOIN dept_manager as dm ON e.emp_no = dm.emp_no
+  JOIN departments as d ON dm.dept_no = d.dept_no
+WHERE dm.to_date = '9999-01-01' AND gender = 'F';
+
+# Find the current titles of employees currently working in the Customer Service department.
+SELECT t.title as Title, count(Title) as Count
+FROM employees AS e
+  JOIN titles AS t ON e.emp_no = t.emp_no
+  JOIN dept_emp AS de ON e.emp_no = de.emp_no
+  JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service' AND de.to_date = '9999-01-01' GROUP BY title;
+
+# Find the current salary of all current managers.
+SELECT d.dept_name as 'Department Name', concat(e.first_name, ' ', e.last_name) as Name, s.salary
+FROM employees AS e
+  JOIN salaries AS s ON e.emp_no = s.emp_no
+  JOIN dept_manager as dm ON s.emp_no = dm.emp_no
+  JOIN departments AS d ON dm.dept_no = d.dept_no
+WHERE s.to_date = '9999-01-01' and dm.to_date = '9999-01-01'
+ORDER BY salary;
+
+# Bonus Find the names of all current employees, their department name, and their current manager's name.
+SELECT concat(e.first_name, ' ' , e.last_name) AS Employee, d.dept_name as Department, concat(managers.first_name, ' ', managers.last_name)
+FROM employees AS e
+  JOIN dept_emp AS de ON e.emp_no = de.emp_no
+  JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
+  JOIN departments AS d ON dm.dept_no = d.dept_no
+  JOIN employees as managers ON dm.emp_no = managers.emp_no
+WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01';
